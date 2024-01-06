b58e48642fd08355be9b8505e0a70c4a4bb7b5f9
Heri Handoko <heri.handoko@mncgroup.com>
2024-01-06 17:01:56 +0700

    M

--- themes/Base/User/Views/frontend/listPackage.blade.php
+++ themes/Base/User/Views/frontend/listPackage.blade.php
@@ -12,7 +12,7 @@
                 <div class="row">
                     <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
                         <div class="bg-image hover-zoom ripple rounded ripple-surface">
-                            <img src="{{ $tour->title }}"class="w-100" />
+                            <img src="https://storage.googleapis.com/muslimpergi/uploads/listing/pict/3222/medium_dauroh.png" width="225" height="150"/>
                             <a href="#!">
                                 <div class="hover-overlay">
                                     <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
@@ -28,40 +28,25 @@
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
                             </div>
-                            <span>145</span>
-                        </div>
-                        <div class="mt-1 mb-0 text-muted small">
-                            <span>100% cotton</span>
-                            <span class="text-primary"> • </span>
-                            <span>Light weight</span>
-                            <span class="text-primary"> • </span>
-                            <span>Best finish<br /></span>
-                        </div>
-                        <div class="mb-2 text-muted small">
-                            <span>Unique design</span>
-                            <span class="text-primary"> • </span>
-                            <span>For women</span>
-                            <span class="text-primary"> • </span>
-                            <span>Casual<br /></span>
                         </div>
                         <p class="text-truncate mb-4 mb-md-0">
-                            There are many variations of passages of Lorem Ipsum available, but the
-                            majority have suffered alteration in some form, by injected humour, or
-                            randomised words which don't look even slightly believable.
+                            <p class="mt-3 mb-1"><i class="simple-icon-plane mr-1"></i>
+                                Dari: <span class="text-primary font-weight-bold ml-1">JAKARTA</span>
+                            </p> 
+                                <p class="mb-3"><i class="simple-icon-calendar mr-1"></i>
+                                Keberangkatan: <span class="text-primary font-weight-bold ml-1">24 Des 2023</span>
+                            </p> 
                         </p>
                     </div>
                     <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
                         <div class="d-flex flex-row align-items-center mb-1">
-                            <h4 class="mb-1 me-1">$17.99</h4>
-                            <span class="text-danger"><s>$25.99</s></span>
+                            <h4 class="mb-1 me-1">{{ number_format($tour->sale_price,0) }}</h4>
                         </div>
-                        <h6 class="text-success">Free shipping</h6>
                         <div class="d-flex flex-column mt-4">
-                            <button class="btn btn-primary btn-sm" type="button">Lihat Detail</button>
-                            <button class="btn btn-outline-primary btn-sm mt-2" type="button">
-                                Pesan Sekarang
-                            </button>
+                            <a class="btn btn-primary btn-sm" href="{{ url('/tour/'.$tour->slug) }}">Lihat Detail</a>
+                            <button class="btn btn-outline-primary btn-sm mt-2" type="button"> Pesan Sekarang </button>
                         </div>
                     </div>
                 </div>
